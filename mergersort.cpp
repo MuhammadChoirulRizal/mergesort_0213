687ddcd14174aebe25b05cc6ad4de4f8243eecae
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-04-15 11:07:39 +0700

    input isi elemnt array

--- mergersort.cpp
+++ mergersort.cpp
@@ -20,6 +20,14 @@ voiid input()
             cout << "\nMaksimal panjang array adalah 20";
 
         }
-        
+
+    }
+    cout << "\n----------"<<endl;
+    cout << "\ninputan isi element array"<< endl;
+    cout << "\n----------"<< endl;
+    for (int i =0; i<n; i++)
+    {
+        cout << "array index ke-" << i <<":";
+        cin >> arr[i];
     }
 )
