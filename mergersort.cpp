9cff70b9ccdcd55e9e07950394f3848d1ce822c8
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-04-15 11:01:18 +0700

    masukkan panjang array

--- mergersort.cpp
+++ mergersort.cpp
@@ -2,3 +2,18 @@
 using namespace std;
 //repository name : mergesort_0213
 //20240140213
+
+int arr [20], B[20];
+int n;
+voiid input()
+(
+    while(true)
+    {
+        cout <<"masukkan panjang element array :";
+        cin >>n;
+        if (n<=20)
+        {
+            break;
+        }
+    }
+)
